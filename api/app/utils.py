f01389f8adbfd8407d20042a8be553b2144a7efe
José Florentino Ramos <joseph0001@gmail.com>
2024-07-04 19:58:16 -0300

    fix API creation

--- api/app/utils.py
+++ api/app/utils.py
@@ -13,4 +13,13 @@ def get_model() -> Pipeline:
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
     transition_dictionary = {key: [value] for key, value in class_model.dict().items()}
     data_frame = DataFrame(transition_dictionary)
+    
+    # Añadir columnas faltantes con valores por defecto
+    missing_columns = {'Unnamed: 0': 0, 'release_date': 'N/A', 'movie_name': 'N/A', 
+                       'domestic_gross': 0.0, 'movie_link': 'N/A'}
+    
+    for column, default_value in missing_columns.items():
+        if column not in data_frame:
+            data_frame[column] = default_value
+    
     return data_frame
