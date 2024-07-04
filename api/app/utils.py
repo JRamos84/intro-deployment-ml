30a35059bd57709a0441e2703961e638e38f1b3b
José Florentino Ramos <joseph0001@gmail.com>
2024-07-04 20:18:08 -0300

    fix api creation

--- api/app/utils.py
+++ api/app/utils.py
@@ -1,17 +1,25 @@
 from joblib import load
 from sklearn.pipeline import Pipeline
-from pydantic import BaseModel
 from pandas import DataFrame
 import os
-from io import BytesIO
+from pydantic import BaseModel
 
 def get_model() -> Pipeline:
     model_path = os.environ.get('MODEL_PATH', 'model/model.pkl')
     with open(model_path, 'rb') as model_file:
-        model = load(BytesIO(model_file.read()))
+        model = load(model_file)
     return model
 
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
-    transition_dictionary = class_model.dict()  # Corregido para usar class_model.dict()
-    data_frame = DataFrame([transition_dictionary])  # Crear DataFrame con una lista que contiene el diccionario
+    transition_dictionary = class_model.model_dump()  # Usar model_dump en lugar de dict
+    data_frame = DataFrame([transition_dictionary])
+    
+    # Añadir columnas faltantes con valores por defecto
+    missing_columns = {'Unnamed: 0': 0, 'release_date': 'N/A', 'movie_name': 'N/A', 
+                       'domestic_gross': 0.0, 'movie_link': 'N/A'}
+    
+    for column, default_value in missing_columns.items():
+        if column not in data_frame:
+            data_frame[column] = default_value
+    
     return data_frame
